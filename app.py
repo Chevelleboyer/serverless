df89f44c6752d6f2b37269e9bffdffef9c0b201f
Chevelle Boyer <chevelleboyer@gmail.com>
2021-03-05 18:35:40 -0800

    Update to work on multiple routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from botocore.client import Config
 import boto3, json
@@ -10,38 +10,126 @@ CORS(app, support_credentials=True)
 @app.route("/")
 @cross_origin(support_credentials=True)
 def hello():
-	s3 = boto3.resource('s3')
-	s3_client = boto3.client('s3')
+	s3 = boto3.resource(
+		's3',
+		region_name='us-east-1',
+		config=Config(signature_version='s3v4')
+	)
+	s3_client = boto3.client('s3',region_name='us-east-1', config=Config(signature_version='s3v4'))
 	bucket = s3.Bucket('s3-bucket-cs493')
-	bucket_contents = bucket.objects.all()
+	bucket_contents = s3_client.list_objects(Bucket='s3-bucket-cs493')["Contents"]
 
-	music = {}
-
-	payload = []
+	songs = []
 	for obj in bucket_contents:
-		song_url = s3_client.generate_presigned_url('get_object',Params={'Bucket':bucket.name,'Key':obj.key}, ExpiresIn=500)
-		info = obj.key.split("/")
-		if len(info) > 2:
-			payload.append([info[0], info[1], info[2], song_url])
-
-	for artist, album, song, song_url in payload:
-		if artist in music:
-			if album in music[artist]:
-				music[artist][album][song] = song_url
-			else:
-				music[artist][album] = {song: song_url}
-		else:
-			music[artist] = {album: {song: song_url}}
+		url = s3_client.generate_presigned_url("get_object",
+			Params={"Bucket": 's3-bucket-cs493', "Key": obj["Key"]}, ExpiresIn=5000,
+		)
+		songs.append(url)
 
 	return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        "body": json.dumps(music, indent=4),
-    }
+		"statusCode": 200,
+		"headers": {
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+		},
+		"body": songs,
+	}
+
+@app.route("/genres")
+def get_genres():
+	ddb_client = boto3.client('dynamodb', region_name="us-west-2")
+	genres = ddb_client.query(
+		TableName="music",
+		KeyConditionExpression='#pk = :pk',
+		ExpressionAttributeNames={
+			'#pk': 'pk'
+		},
+		ExpressionAttributeValues={
+			':pk': { 'S': 'genre' },
+		}
+	)
+	return json.dumps([genre["name"]["S"] for genre in genres["Items"]])
+
+@app.route("/artists/for/genre")
+def get_artists_for_genre():
+	genre = request.args.get("genre")
+	ddb_client = boto3.client('dynamodb', region_name="us-west-2")
+	artists = ddb_client.query(
+		TableName="music",
+		KeyConditionExpression='#pk = :pk and begins_with(#sk, :sk)',
+		ExpressionAttributeNames={
+			'#pk': 'pk',
+			"#sk": "sk",
+		},
+		ExpressionAttributeValues={
+			':pk': { 'S': 'genre#'+genre },
+			':sk': {'S': 'artist'},
+		}
+	)
+	return json.dumps([artist["name"]["S"] for artist in artists["Items"]])
+
+@app.route("/albums/for/artist")
+def get_albums_for_artist():
+	artist = request.args.get("artist")
+	ddb_client = boto3.client('dynamodb', region_name="us-west-2")
+	albums = ddb_client.query(
+		TableName="music",
+		KeyConditionExpression='#pk = :pk and begins_with(#sk, :sk)',
+		ExpressionAttributeNames={
+			'#pk': 'pk',
+			"#sk": "sk",
+		},
+		ExpressionAttributeValues={
+			':pk': { 'S': 'artist#'+artist },
+			':sk': {'S': 'album'},
+		}
+	)
+	return json.dumps([album["name"]["S"] for album in albums["Items"]])
+
+@app.route("/songs/for/album")
+def get_songs_for_album():
+	album = request.args.get("album")
+	ddb_client = boto3.client('dynamodb', region_name="us-west-2")
+	songs = ddb_client.query(
+		TableName="music",
+		KeyConditionExpression='#pk = :pk and begins_with(#sk, :sk)',
+		ExpressionAttributeNames={
+			'#pk': 'pk',
+			"#sk": "sk",
+		},
+		ExpressionAttributeValues={
+			':pk': { 'S': 'album#'+album },
+			':sk': {'S': 'song'},
+		}
+	)
+	return json.dumps([song["name"]["S"] for song in songs["Items"]])
+
+@app.route("/song")
+def get_song():
+	song = request.args.get("song")
+	ddb_client = boto3.client('dynamodb', region_name="us-west-2")
+	song_obj = ddb_client.query(
+		TableName="music",
+		KeyConditionExpression='#pk = :pk and #sk = :sk',
+		ExpressionAttributeNames={
+			'#pk': 'pk',
+			"#sk": "sk",
+		},
+		ExpressionAttributeValues={
+			':pk': { 'S': 'song' },
+			':sk': {'S': song},
+		}
+	)
+
+	s3_key = song_obj["Items"][0]["s3_key"]["S"]
+	s3_client = boto3.client('s3',region_name='us-east-2', config=Config(signature_version='s3v4'))
+	url = s3_client.generate_presigned_url("get_object",
+		Params={"Bucket": 's3-bucket-cs493', "Key": s3_key}, ExpiresIn=5000,
+	)
+
+	return url
+	
 
 
 
